476c44f5815549fbd883b9fd31efd3a2d00d3af4
Dragontore <dragontore@hotmail.co.uk>
2019-11-23 15:37:41 +0000

    Character Movement Added and Base Stats Component

---
+++ Source/RPG/Private/Components/BaseStatsComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseStatsComponent.h"
+
+// Sets default values for this component's properties
+UBaseStatsComponent::UBaseStatsComponent()
+{
+
+}
+
+
+// Called when the game starts
+void UBaseStatsComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
