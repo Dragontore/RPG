b3fb0a5a3056498cbbd4d9cc56155afb06631e75
Dragontore <dragontore@hotmail.co.uk>
2019-11-23 16:42:33 +0000

    snecky kitty video 3 11.21 mins

--- Source/RPG/Private/Components/BaseStatsComponent.cpp
+++ Source/RPG/Private/Components/BaseStatsComponent.cpp
@@ -3,10 +3,18 @@
 
 #include "BaseStatsComponent.h"
 
+#include "Net/UnrealNetwork.h"
+#include "Engine/World.h"
+#include "Engine/Engine.h"
+#include "TimerManager.h"
+
+
+
 // Sets default values for this component's properties
 UBaseStatsComponent::UBaseStatsComponent()
 {
-
+	CurrentHealth = 50.f;
+	MaxHealth = 100.f;
 }
 
 
@@ -15,8 +23,27 @@ void UBaseStatsComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	SetIsReplicated(true);
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UBaseStatsComponent::HandleBaseStats, RegenRate, true);
 	
 }
 
+void UBaseStatsComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	// Replicate to every client, no special condition required
+	DOREPLIFETIME(UBaseStatsComponent, CurrentHealth);
+	DOREPLIFETIME(UBaseStatsComponent, MaxHealth);
+}
+
+void UBaseStatsComponent::HandleBaseStats()
+{
+
+}
+
+void UBaseStatsComponent::HealthRegen(float healthRegen)
+{
+
+}
--- Source/RPG/Public/Components/BaseStatsComponent.h
+++ Source/RPG/Public/Components/BaseStatsComponent.h
@@ -12,6 +12,21 @@ class RPG_API UBaseStatsComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+public:
+
+protected:
+
+	FTimerHandle TimerHandle;
+
+	UPROPERTY(Replicated)
+	float CurrentHealth;
+
+	UPROPERTY(Replicated)
+	float MaxHealth;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "BaseStats")
+	float RegenRate;
+
 public:	
 	// Sets default values for this component's properties
 	UBaseStatsComponent();
@@ -20,6 +35,11 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	//Handles the bas Stats regen
+	void HandleBaseStats();
+
 public:	
+
+	void HealthRegen(float healthRegen);
 	
 };
