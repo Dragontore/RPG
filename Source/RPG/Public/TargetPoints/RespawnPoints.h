4f41ad143c7816dba5c87512fce8ff1a2621ad2f
Dragontore <dragontore@hotmail.co.uk>
2020-01-02 10:17:18 +0000

    target points added

---
+++ Source/RPG/Public/TargetPoints/RespawnPoints.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "RespawnPoints.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API ARespawnPoints : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
