2199831a329a2f8034758b7db55d31f46a64dfa4
Dragontore <dragontore@hotmail.co.uk>
2020-02-16 17:34:43 +0000

    transfer buttons sorted, Base Room added

---
+++ Source/RPG/Private/Rooms/BaseRoom.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseRoom.h"
+
+// Sets default values
+ABaseRoom::ABaseRoom()
+{
+
+
+}
+
+// Called when the game starts or when spawned
+void ABaseRoom::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
